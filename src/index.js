301737122e553f05c92f270ef93d3a479b768f5d
Isabellukk <isabellukk@gmail.com>
2022-07-19 07:36:14 -0700

    Fixed route

--- src/index.js
+++ src/index.js
@@ -7,12 +7,15 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<App />}>
-					<Route path="/:id" element={<UniversitySearch />} />
+					<Route path="/search" element={<UniversitySearch />}>
+						<Route path="/search:id" element={<UniversitySearch />} />
+					</Route>
 				</Route>
 			</Routes>
 		</BrowserRouter>
