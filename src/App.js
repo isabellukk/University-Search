10607005785f7ff9089733d539df90dd6c91e0c4
Isabellukk <isabellukk@gmail.com>
2022-07-22 00:46:00 -0700

    migrated react router functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import UniversitySearch from "./components/UniversitySearch.js";
 import "./App.css";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import UniversitySearch from "./components/UniversitySearch";
 
 function App() {
 	return (
 		<div className="App">
-			<UniversitySearch />
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<UniversitySearch />}>
+						<Route path="/search" element={<UniversitySearch />}>
+							<Route path="/search:id" element={<UniversitySearch />} />
+						</Route>
+					</Route>
+				</Routes>
+			</BrowserRouter>
 		</div>
 	);
 }
